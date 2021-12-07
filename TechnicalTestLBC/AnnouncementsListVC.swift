3abb856c2bd5c770f7c16f9ca51f19fb4bab6b0b
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-07 09:51:22 +0800

    set delegate and data source then call method in configure method

--- TechnicalTestLBC/AnnouncementsListVC.swift
+++ TechnicalTestLBC/AnnouncementsListVC.swift
@@ -19,5 +19,29 @@ class AnnouncementsListVC: UIViewController {
     
     func configureTableView() {
         view.addSubview(tableView)
+            // set delegates
+            //set row and height
+            // Register cells
+            // set contraints
     }
+    
+    func setTableViewDelegaates() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+}
+
+extension AnnouncementsListVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
+extension AnnouncementsListVC: UITableViewDelegate {
+    
 }
