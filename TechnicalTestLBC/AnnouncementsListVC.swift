114fe0f0b8fd2aa3e86ed77a4dbfadfd60ab9c67
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-07 16:12:08 +0800

    set method for fetch data

--- TechnicalTestLBC/AnnouncementsListVC.swift
+++ TechnicalTestLBC/AnnouncementsListVC.swift
@@ -19,12 +19,10 @@ class AnnouncementsListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Liste d'annonce"
-        fetchData()
+        fetchData(onCompletion: fetchData())
         configureTableView()
         setTableViewDelegates()
     }
-    
-   
     
     func configureTableView() {
         view.addSubview(tableView)
@@ -36,51 +34,59 @@ class AnnouncementsListVC: UIViewController {
     
     func setTableViewDelegates() {
         tableView.dataSource = self
-        tableView.delegate = self
-    }    
+    }
+    
+    func fetchData() -> ([Response]) -> () {
+        let anonymousFunction = { (fetchedResults: [Response]) in
+            DispatchQueue.main.async {
+                self.result = fetchedResults
+                self.tableView.reloadData()
+            }
+        }
+        
+        return anonymousFunction
+    }
 }
 
-extension AnnouncementsListVC: UITableViewDataSource, UITableViewDelegate {
+extension AnnouncementsListVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return result.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: Cells.AnnouncementCell, for: indexPath) as! AnnouncmentCell
-
-        let announcement = self.result[indexPath.row]
-        print("*** dequeue \(announcement)")
-        cell.set(response: announcement)
+        let cell = tableView.dequeueReusableCell(withIdentifier: Cells.AnnouncementCell, for: indexPath)
+        //
+        let item = result[indexPath.row].title
+        cell.textLabel?.text = item
         return cell
         
     }
-    
 }
 
 extension AnnouncementsListVC {
     
-    func fetchData() {
+    func fetchData(onCompletion: @escaping ([Response]) -> ()) {
         guard let url = URL(string: "https://raw.githubusercontent.com/leboncoin/paperclip/master/listing.json") else {
             print("Invalid URL")
             return
         }
         
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data, error == nil else {
-                print("Something went wrong")
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                print("Data was nil")
                 return
             }
             
             // We have data
-            do {
-                self.result = try JSONDecoder().decode([Response].self, from: data)
-            } catch {
-                print("Failed to convert \(error.localizedDescription)")
-                print(String(describing: error))
+            
+            guard let decodedResponse = try? JSONDecoder().decode([Response].self, from: data) else {
+                print("Couldn't decode json")
+                return
             }
             
-            print(self.result)
+            onCompletion(decodedResponse)
         }
+        
         task.resume()
     }
 }
