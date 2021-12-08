41f6c0fb4b97df9fd4be49c9afa07d1e13167178
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-08 15:12:10 +0800

    set a method for category id

--- TechnicalTestLBC/AnnouncementsListVC.swift
+++ TechnicalTestLBC/AnnouncementsListVC.swift
@@ -45,6 +45,35 @@ class AnnouncementsListVC: UIViewController {
         }
         return anonymousFunction
     }
+    
+    func getCategoryDescription(id: Int) -> String {
+        switch id {
+        case 1:
+            return "Vehicule"
+        case 2:
+            return "Mode"
+        case 3:
+            return "Bricolage"
+        case 4:
+            return "Maison"
+        case 5:
+            return "Loisirs"
+        case 6:
+            return "Immobilier"
+        case 7:
+            return "Livres/CD/DVD"
+        case 8:
+            return "Multimédia"
+        case 9:
+            return "Service"
+        case 10:
+            return "Animaux"
+        case 11:
+            return "Enfants"
+        default:
+            return "Catégorie introuvable"
+        }
+    }
 }
 
 extension AnnouncementsListVC: UITableViewDataSource {
@@ -61,6 +90,7 @@ extension AnnouncementsListVC: UITableViewDataSource {
         itemCell.titleLabel.text = item.title
         itemCell.priceLabel.text = "\(item.price.stringWithoutZeroFraction) €"
         itemCell.isUrgentLabel.text = item.isUrgent == true ? "Urgent" : ""
+        itemCell.categoryLabel.text = getCategoryDescription(id: item.categoryID)
         return cell
         
     }
