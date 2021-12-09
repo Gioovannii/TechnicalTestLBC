8eb3bf529811d11c723aadc9e5acc3d5b7c1cf67
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-09 14:16:47 +0800

    set some constant to store strings

---
+++ TechnicalTestLBC/Model/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  TechnicalTestLBC
+//
+//  Created by Giovanni Gaffé on 2021/12/9.
+//
+
+import Foundation
+
+struct Cells {
+    static let AnnouncementCell = "AnnouncementCell"
+}
