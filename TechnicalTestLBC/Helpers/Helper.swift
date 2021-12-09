ce659117c33e8216cc045aeeed82c605d1405f29
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-09 18:57:01 +0800

    remove unused helpers and creation file helper

---
+++ TechnicalTestLBC/Helpers/Helper.swift
@@ -0,0 +1,39 @@
+//
+//  CategoriesDescription.swift
+//  TechnicalTestLBC
+//
+//  Created by Giovanni Gaffé on 2021/12/9.
+//
+
+import Foundation
+
+struct Helper {
+    static func getCategoryDescription(id: Int) -> String {
+        switch id {
+        case 1:
+            return "Vehicule"
+        case 2:
+            return "Mode"
+        case 3:
+            return "Bricolage"
+        case 4:
+            return "Maison"
+        case 5:
+            return "Loisirs"
+        case 6:
+            return "Immobilier"
+        case 7:
+            return "Livres/CD/DVD"
+        case 8:
+            return "Multimédia"
+        case 9:
+            return "Service"
+        case 10:
+            return "Animaux"
+        case 11:
+            return "Enfants"
+        default:
+            return "Catégorie introuvable"
+        }
+    }
+}
