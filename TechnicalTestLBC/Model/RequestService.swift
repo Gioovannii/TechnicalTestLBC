2a0730141dc7340358d9f3172ecc8e35a483a344
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-13 11:28:21 +0800

    refactor Service with result type

--- TechnicalTestLBC/Model/RequestService.swift
+++ TechnicalTestLBC/Model/RequestService.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 protocol RequestTestService {
-    func fetchData(onCompletion: @escaping ([Response]) -> Void)
+    func fetchData(onCompletion: @escaping (Result<[Response], NetworkError>) -> Void)
 }
 
 final class RequestService: RequestTestService {
@@ -18,23 +18,32 @@ final class RequestService: RequestTestService {
         self.session = session
     }
     
-    func fetchData(onCompletion: @escaping ([Response]) -> Void) {
+    func fetchData(onCompletion: @escaping (Result<[Response], NetworkError>) -> Void) {
         guard let url = URL(string: "https://raw.githubusercontent.com/leboncoin/paperclip/master/listing.json") else {
             print("Invalid URL")
             return
         }
         session.dataTask(with: url) { (data, response, error) in
             guard let data = data else {
+
+                onCompletion(.failure(.noData))
                 print("Data was nil")
                 return
             }
             
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                onCompletion(.failure(.incorrectResponse))
+                print("No response")
+                return
+            }
+            
             guard let decodedResponse = try? JSONDecoder().decode([Response].self, from: data) else {
+                onCompletion(.failure(.undecodableData))
                 print("Couldn't decode json")
                 return
             }
             
-            onCompletion(decodedResponse)
+            onCompletion(.success(decodedResponse))
         }
         .resume()
     }
