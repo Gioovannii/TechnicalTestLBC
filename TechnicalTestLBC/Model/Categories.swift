8f44afc7fb6acd12415464217c945c1abb81cb67
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-12-15 16:13:25 +0800

    set a codable Category

---
+++ TechnicalTestLBC/Model/Categories.swift
@@ -0,0 +1,14 @@
+//
+//  Categories.swift
+//  TechnicalTestLBC
+//
+//  Created by Giovanni Gaffé on 2021/12/15.
+//
+
+import Foundation
+
+// MARK: - Category
+struct Category: Codable {
+    let id: Int
+    let name: String
+}
